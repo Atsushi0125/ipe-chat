d106e13d56260b31bb3db09508b1ccbe19ce6b8c
Atsushi <katoatsushi0125@gmail.com>
2020-08-26 18:40:15 +0900

    第11章

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,13 @@
 class MessagesController < ApplicationController
 
     def index
-        @messages = Message.all
+        @messages = Message.all.includes(:user)
         @message = Message.new
     end
 
     def create
         message = Message.new(create_params)
-        if @message.save
+        if message.save
             redirect_to root_path, notice: 'メッセージを送信しました'
         else
             redirect_to root_path, notice: 'メッセージの送信に失敗しました'
@@ -16,11 +16,14 @@ class MessagesController < ApplicationController
 
     def edit
         @message = Message.find(params[:id])
+        unless @message.user_id == current_user.id
+            redirect_to root_path, notice: '不正なアクセスです'
+        end
     end
 
     def update
         @message = Message.find(params[:id])
-        if @message.update(update_params)
+        if @message.user_id == current_user.id && @message.update(update_params)
             redirect_to root_path, notice: '変更しました'
         else
             redirect_to root_path, notice: '変更に失敗しました'
@@ -39,7 +42,7 @@ class MessagesController < ApplicationController
     private
 
     def create_params
-        params.require(:message).permit(:content).merge(user_id: 1)
+    	params.require(:message).permit(:content).merge(user_id: current_user.id)
     end
 
     def update_params
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,11 @@ Rails.application.routes.draw do
   get "messages/:id/edit", to: "messages#edit", as: "messages_edit"
   patch "messages/:id/update", to: "messages#update", as: "messages_update"
   delete "messages/:id/delete", to: "messages#delete", as: "messages_delete"
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#login'
+  
+  get '/signup', to: 'sessions#signup'
+  post '/signup', to: 'sessions#create'
 
 end
